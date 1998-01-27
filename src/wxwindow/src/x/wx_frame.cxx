0d579141141f4844853cc5ca3d0e1cf82866d2eb
mflatt <mflatt>
1998-01-27 17:03:40 +0000

    force-focus

--- src/wxwindow/src/x/wx_frame.cxx
+++ src/wxwindow/src/x/wx_frame.cxx
@@ -4,7 +4,7 @@
  * Author:	Julian Smart
  * Created:	1993
  * Updated:	August 1994
- * RCS_ID:      $Id: wx_frame.cxx,v 1.1.1.1 1997/12/22 16:12:04 mflatt Exp $
+ * RCS_ID:      $Id: wx_frame.cxx,v 1.2 1998/01/27 17:03:40 mflatt Exp $
  * Copyright:	(c) 1993, AIAI, University of Edinburgh
  */
 
@@ -544,6 +544,33 @@ void wxFrame::SetSize(int x, int y, int width, int height, int sizeFlags)
   GetSize(&lastWidth, &lastHeight);
 }
 
+static void ForceFocus(Widget frame)
+{
+  static int force_focus = 0;
+
+  if (!force_focus) {
+    wxGetResource(wxTheApp->wx_class, "forceFocus", &force_focus);
+    force_focus = !force_focus ? -1 : 1;
+  }
+
+  if (force_focus > 0) {
+    Window current;
+    int old_revert;
+    XGetInputFocus(XtDisplay(frame), &current, &old_revert);
+    if (current != PointerRoot) {
+      XFlush(XtDisplay(frame));
+      XGrabServer(XtDisplay(frame));
+      
+      XWindowAttributes attrib;
+      XGetWindowAttributes(XtDisplay(frame), XtWindow(frame), &attrib);
+      if (attrib.map_state == IsViewable)
+	XSetInputFocus(XtDisplay(frame), XtWindow(frame),
+		       RevertToNone, CurrentTime);
+    }
+    XUngrabServer(XtDisplay(frame));
+  }
+}
+
 Bool wxFrame::Show(Bool show)
 {
   SetShown(show);
@@ -566,6 +593,7 @@ Bool wxFrame::Show(Bool show)
   if (show) {
     XtMapWidget(frameShell);
     XRaiseWindow(XtDisplay(frameShell), XtWindow(frameShell));
+    ForceFocus(frameShell);
   } else {
     XtUnmapWidget(frameShell);
 //    XmUpdateDisplay(wxTheApp->topLevel); // Experimental: may be responsible for crashes
