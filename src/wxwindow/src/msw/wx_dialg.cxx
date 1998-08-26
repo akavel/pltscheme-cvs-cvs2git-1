459c530fb5d819ee5be7fca3ea367cd0f9cf8b59
mflatt <mflatt>
1998-08-26 19:05:26 +0000

    .

--- src/wxwindow/src/msw/wx_dialg.cxx
+++ src/wxwindow/src/msw/wx_dialg.cxx
@@ -4,7 +4,7 @@
  * Author:	Julian Smart
  * Created:	1993
  * Updated:	August 1994
- * RCS_ID:      $Id: wx_dialg.cxx,v 1.5 1998/08/10 12:53:32 mflatt Exp $
+ * RCS_ID:      $Id: wx_dialg.cxx,v 1.6 1998/08/26 19:05:26 mflatt Exp $
  * Copyright:	(c) 1993, AIAI, University of Edinburgh
  */
 
@@ -208,9 +208,9 @@ Bool wxDialogBox::Create(wxWindow *Parent, char *Title, Bool Modal,
   modal_showing = FALSE;
 
   if (width < 0)
-    width = 500;
+    width = 0; // was 500
   if (height < 0)
-    height = 500;
+    height = 0; // was 500
 
   // Allows creation of dialogs with & without captions under MSWindows
   wxDialogWnd *wnd;
