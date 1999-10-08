21685224e8ab747a636a87d6bf49ba14b0f12d4a
mflatt <mflatt>
1999-10-08 04:36:35 +0000

    .

--- src/wxxt/src/Windows/Choice.cc
+++ src/wxxt/src/Windows/Choice.cc
@@ -1,5 +1,5 @@
 /*								-*- C++ -*-
- * $Id: Choice.cc,v 1.14 1999/10/08 04:33:18 mflatt Exp $
+ * $Id: Choice.cc,v 1.15 1999/10/08 04:36:35 mflatt Exp $
  *
  * Purpose: choice panel item
  *
@@ -121,7 +121,8 @@ Bool wxChoice::Create(wxPanel *panel, wxFunction function, char *label,
       for (int i = 0; i < n; i++) {
 	float w, h;
 	GetTextExtent(choices[i], &w, &h, NULL, NULL, label_font);
-	maxw = w;
+	if (w > maxw)
+	  maxw = w;
       }
       
       if (label && !vert) {
