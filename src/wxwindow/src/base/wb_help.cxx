56f7f888a23389a8308b37ef00fd1d140b1c8fd8
mflatt <mflatt>
2000-03-16 02:28:16 +0000

    .

--- src/wxwindow/src/base/wb_help.cxx
+++ src/wxwindow/src/base/wb_help.cxx
@@ -4,16 +4,7 @@
  * Author:	Julian Smart
  * Created:	1993
  * Updated:	August 1994
- * RCS_ID:      $Id: wb_help.cxx,v 1.2 1998/07/13 19:08:15 mflatt Exp $
  * Copyright:	(c) 1993, AIAI, University of Edinburgh
  */
 
-// #include "wx.h" // Uncomment this line for Borland precomp. headers to work
-
-/* static const char sccsid[] = "@(#)wb_help.cc	1.2 5/9/94"; */
-
-#if defined(_MSC_VER)
-# include "wx.h"
-#else
-
-#endif // USE_HELP
+#include "wx.h"
