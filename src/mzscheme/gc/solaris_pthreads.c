1f903617c4b5c95df1e80a6680f684961ff74611
mflatt <mflatt>
2001-03-20 23:33:35 +0000

    .

--- src/mzscheme/gc/solaris_pthreads.c
+++ src/mzscheme/gc/solaris_pthreads.c
@@ -16,7 +16,7 @@
  * Modified Peter C. for Solaris Posix Threads.
  */
 /* Boehm, September 14, 1994 4:44 pm PDT */
-/* $Id: solaris_pthreads.c,v 1.5 2000/11/03 18:26:24 mflatt Exp $ */
+/* $Id: solaris_pthreads.c,v 1.6 2001/03/20 23:32:25 mflatt Exp $ */
 
 # if defined(GC_SOLARIS_PTHREADS) || defined(_SOLARIS_PTHREADS)
 # include "private/gc_priv.h"
