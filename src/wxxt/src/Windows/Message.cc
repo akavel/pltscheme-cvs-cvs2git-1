60a4f9d0e6d38300f0c581f59d41a3402319b6c9
mflatt <mflatt>
2002-09-04 14:28:06 +0000

    .

--- src/wxxt/src/Windows/Message.cc
+++ src/wxxt/src/Windows/Message.cc
@@ -1,5 +1,5 @@
 /*								-*- C++ -*-
- * $Id: Message.cc,v 1.10 2002/09/04 13:24:11 mflatt Exp $
+ * $Id: Message.cc,v 1.11 2002/09/04 14:26:55 mflatt Exp $
  *
  * Purpose: message panel item
  *
@@ -363,6 +363,15 @@ void wxMessage::SetLabel(char *message)
 
 void wxMessage::SetLabel(wxBitmap *bitmap)
 {
+  {
+    /* Don't reset the label if this is an icon message: */
+    int i;
+    for (i = 0; i < 3; i++) {
+      if (bm_label == icons[i])
+	return;
+    }
+  }
+
   if (bm_label && bitmap && bitmap->Ok() && (bitmap->selectedIntoDC >= 0)
       && (bitmap->GetDepth()==1 || bitmap->GetDepth()==wxDisplayDepth())) {
     Pixmap pm;
